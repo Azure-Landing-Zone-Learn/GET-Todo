6d6124be2b398eb5e7fe44640946ce4d28b12aed
tungUAS <tung.tt133@gmail.com>
2024-11-01 22:44:33 +0100

    make pipeline reuseable

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const APP_PORT = process.env.APP_PORT || 80;
 
-let todos = [
-  { id: 1, task: "Buy groceries", completed: false },
-  { id: 2, task: "Clean the house", completed: true },
-  { id: 3, task: "Finish homework", completed: false }
-];
-
 app.get('/todoapi/todos', (req, res) => {
   res.status(200).json({
-    data: todos
+    data: [
+      { id: 1, task: "Buy groceries", completed: false },
+      { id: 2, task: "Clean the house", completed: true },
+      { id: 3, task: "Finish homework", completed: false }
+    ]
   });
 });
 
