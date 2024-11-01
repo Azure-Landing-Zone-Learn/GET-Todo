5ba5bbb41da1aabd7f6af94ee00a70cb0343dc58
tungUAS <tung.tt133@gmail.com>
2024-11-01 23:28:26 +0100

    make pipeline reuseable

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/todoapi/todos', (req, res) => {
   });
 });
 
-// hehehehehe
+// hehehehehehe
 app.listen(APP_PORT, () => {
   console.log(`Server running on http://localhost:${APP_PORT}`);
 });
